dfaa7f9e2387990467176f225c24b1215303ce0c
Zaid Ghaffar <144371876+ZaidGhaffar@users.noreply.github.com>
2025-03-18 08:55:33 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,6 +20,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          id="spline-preload"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              const preloadSpline = new Image();
+              preloadSpline.src = "https://prod.spline.design/kZDDjO5HuC9GJUM2/scene.splinecode";
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
